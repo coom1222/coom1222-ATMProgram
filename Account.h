591c3212a76381a097bd9c8ec487387241af4108
junmin <coom1222@gmail.com>
2024-04-12 22:08:45 +0900

    보기좋게 커밋

--- Account.h
+++ Account.h
@@ -6,16 +6,16 @@ using std::string;
 #define BASE_ACCOUNT_ID 100 // 계좌번호는 100번 부터 999 사이 랜덤으로 부여
 class Account {
 private:
-int nID; // 계좌 번호 (초기 값 = -1) , 계좌 해지시 초기값으로 변경
-int nBalance; // 잔고 (초기 값 = 0)
-string strAccountName; // 고객 명
-string strPassword; // 계좌 비밀번호
+    int nID; // 계좌 번호 (초기 값 = -1) , 계좌 해지시 초기값으로 변경
+    int nBalance; // 잔고 (초기 값 = 0)
+    string strAccountName; // 고객 명
+    string strPassword; // 계좌 비밀번호
 private:
-inline bool authenticate(int id, string passwd); // 인증 성공 : true(1), 인증 실패 : false(0)
+    inline bool authenticate(int id, string passwd); // 인증 성공 : true(1), 인증 실패 : false(0)
 public:
-Account();
-void create(int id, int money, string name, string password); // 계좌 개설
-int check(int id, string password); // return 값 : nBalance (잔고) or 인증 실패(AUTHENTIFICATION_FAIL)
-int getAcctID() { return nID; } // 계좌 번호 읽어오기
+    Account();
+    void create(int id, int money, string name, string password); // 계좌 개설
+    int check(int id, string password); // return 값 : nBalance (잔고) or 인증 실패(AUTHENTIFICATION_FAIL)
+    int getAcctID() { return nID; } // 계좌 번호 읽어오기
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -1,19 +1,22 @@
+#include <iostream>
+using namespace std;
+
 int main() {
-int select;
-// 고객 수(100명), ATM 잔금 초기화, 관리자 암호
-ATMachine atm(100, 50000, “admin”);
-while(1) {
-atm.displayMenu();
-cout << “메뉴를 선택하세요: “ ;
-cin>>select;
-cout<<endl;
-switch(select) {
-case 1:
-atm.createAccount();
-break;
-………………………………………………………
-default:
-cout<<"번호 확인 후 다시 입력하세요."<<endl;
+    int select;
+    // 고객 수(100명), ATM 잔금 초기화, 관리자 암호
+    ATMachine atm(100, 50000, "admin");
+    while (1) {
+        atm.displayMenu();
+        cout <<"메뉴를선택하세요: ";
+        cin >> select;
+        cout << endl;
+        switch (select) {
+            case 1:
+                atm.createAccount();
+                break;
+
+            default:
+                cout << "번호 확인 후 다시 입력하세요." << endl;
+        }
+    }
 }
-}
-r
