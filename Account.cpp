4651309c932f824d48d5ccfe6603c6c116dc3d00
junmin <coom1222@gmail.com>
2024-05-09 20:43:01 +0900

    계좌 해지기능 구현 완료

--- Account.cpp
+++ Account.cpp
@@ -1,15 +1,15 @@
-//
-// Created by 준민쓰 MacBook Air on 4/12/24.
-//
-// 여긴 feature 브랜치에요
 #include <iostream>
 // #include "ATMachine.h"
 #include "Account.h"
-
 using namespace std;
 
+
+Account::Account() {
+    nID = -1;
+    nBalance = 0;
+}
 //계좌개설
-void Account::create(int id, int money, std::string name, std::string password) {
+void Account::create(int id, int money, string& name, string& password) {
     nID = id;
     nBalance = money;
     strAccountName = name;
@@ -17,21 +17,26 @@ void Account::create(int id, int money, std::string name, std::string password) 
 }
 
 //return값:nBalance(잔고) or 인증실패(AUTHENTIFICATION_FAIL)
-int Account::check(int id, string password) {
-    int authResult = authenticate(id, password); // 인증 결과를 int로 받음
-    if (authResult == AUTHENTIFICATION_SUCCESS) { // 인증 성공시
-        return nBalance; // 현재 잔액 반환
-    } else { // 인증 실패시
-        return AUTHENTIFICATION_FAIL; // 인증 실패 상수 반환
+int Account::check(int id, string& password) {
+    if (nID == id && strPassword == password) {
+        return nBalance;
+    } else {
+        return AUTHENTIFICATION_FAIL;
     }
 }
 
-// 인증 메소드
-// 인증 메서드 수정본
-int Account::authenticate(int id, string passwd) {
-    if ((nID == id) && (strPassword == passwd)) {
-        return AUTHENTIFICATION_SUCCESS; // 인증 성공
-    } else {
-        return AUTHENTIFICATION_FAIL; // 인증 실패
+
+
+
+void Account::close() {
+    if (nBalance == 0) {
+        nID = -1;
+        strAccountName = "";
+        strPassword = "";
     }
 }
+
+int Account::getBalance() {
+    return nBalance;
+}
+
--- Account.h
+++ Account.h
@@ -12,11 +12,13 @@ private:
     string strAccountName; // 고객 명
     string strPassword; // 계좌 비밀번호
 private:
-    inline int authenticate(int id, string passwd); // 인증 성공 : true(1), 인증 실패 : false(0)
+    //inline int authenticate(int id, string passwd); // 인증 성공 : true(1), 인증 실패 : false(0)
 public:
     Account();
-    void create(int id, int money, string name, string password); // 계좌 개설
-    int check(int id, string password); // return 값 : nBalance (잔고) or 인증 실패(AUTHENTIFICATION_FAIL)
-    int getAcctID() { return nID; } // 계좌 번호 읽어오기
+    void create(int id, int money, string& name, string& password); // 계좌 개설
+    int check(int id, string& password); // return 값 : nBalance (잔고) or 인증 실패(AUTHENTIFICATION_FAIL)
+    int getID() { return nID; } // 계좌 번호 읽어오기
+    void close();   //잔액이 존재하면 해지 불가.
+    int getBalance();
 };
 #endif
